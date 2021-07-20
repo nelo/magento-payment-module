5e0759d6c34bcc684c04a45271d4a13ad7a56ea7
Nelson González Peñate <npenate@users.noreply.github.com>
2021-07-20 12:39:42 -0500

    Add as low as message in checkout (#1)

--- Model/NeloConfigProvider.php
+++ Model/NeloConfigProvider.php
@@ -3,6 +3,7 @@
 namespace Nelo\Bnpl\Model;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Payment\Gateway\ConfigInterface;
 use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Payment\Helper\Data as PaymentHelper;
@@ -15,24 +16,24 @@ use Magento\Payment\Helper\Data as PaymentHelper;
 class NeloConfigProvider implements ConfigProviderInterface
 {
     /**
-     * Nelo Logo
-     */
-    const NELO_LOGO_SRC = 'https://assets.website-files.com/60738c4cc5ee7fcb4d666020/607861e85ed0f047531fd842_wordmark.svg';
-
-    /**
      * @var ResolverInterface
      */
-    protected $localeResolver;
+    protected ResolverInterface $localeResolver;
 
     /**
      * @var PaymentHelper
      */
-    protected $paymentHelper;
+    protected PaymentHelper $paymentHelper;
 
     /**
      * @var UrlInterface
      */
-    protected $urlBuilder;
+    protected UrlInterface $urlBuilder;
+
+    /**
+     * @var ConfigInterface
+     */
+    private ConfigInterface $config;
 
     /**
      * NeloConfigProvider constructor.
@@ -44,11 +45,13 @@ class NeloConfigProvider implements ConfigProviderInterface
     public function __construct(
         ResolverInterface $localeResolver,
         PaymentHelper $paymentHelper,
-        UrlInterface $urlBuilder
+        UrlInterface $urlBuilder,
+        ConfigInterface $config
     ) {
         $this->localeResolver = $localeResolver;
         $this->paymentHelper  = $paymentHelper;
         $this->urlBuilder     = $urlBuilder;
+        $this->config         = $config;
     }
 
     /**
@@ -59,8 +62,9 @@ class NeloConfigProvider implements ConfigProviderInterface
         return [
             'payment' => [
                 'bnpl' => [
-                    'redirectUrl' => $this->urlBuilder->getUrl('nelo/payment/start'),
-                    'logoSrc' => self::NELO_LOGO_SRC
+                    'redirectUrl'       => $this->urlBuilder->getUrl('nelo/payment/start'),
+                    'publishableApiKey' => $this->config->getValue('publishable_api_key'),
+                    'isSandboxMode'     => (bool)$this->config->getValue('sandbox_flag')
                 ]
             ]
         ];
