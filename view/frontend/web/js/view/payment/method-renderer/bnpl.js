3ad425574be020e3cbdc2b2749582f013fd22757
Nelson Gonzalez <nelson@nelo.co>
2021-07-20 14:00:36 -0500

    Load script in method-renderer

--- view/frontend/web/js/view/payment/method-renderer/bnpl.js
+++ view/frontend/web/js/view/payment/method-renderer/bnpl.js
@@ -3,8 +3,9 @@ define([
     'Magento_Checkout/js/view/payment/default',
     'Magento_Paypal/js/action/set-payment-method',
     'Magento_Checkout/js/model/payment/additional-validators',
-    'Nelo_Bnpl/js/action/redirect-on-success'
-    ], function ($, Component, setPaymentMethodAction, additionalValidators, redirectOnSuccessAction) {
+    'Nelo_Bnpl/js/action/redirect-on-success',
+    "Magento_Checkout/js/model/quote"
+    ], function ($, Component, setPaymentMethodAction, additionalValidators, redirectOnSuccessAction, quote) {
         'use strict';
 
         return Component.extend({
@@ -14,6 +15,65 @@ define([
             redirectAfterPlaceOrder: true,
             placeOrderHandler: null,
             validateHandler: null,
+
+            initialize: function() {
+                self = this;
+                self._super();
+
+                if (typeof nelo == "undefined") {
+                    $.when(self._loadScripts()).done(function() {
+                        self._setPrice();
+                    });
+                } else {
+                    self._setPrice();
+                }
+                quote.totals.subscribe(function(newValue) {
+                    self._setPrice(newValue);
+                });
+            },
+
+            _setPrice: function(newValue) {
+                self = this;
+                let price = quote.getTotals()();
+                if (newValue) {
+                    price = newValue;
+                }
+                let result = 0;
+                if (price && price.base_grand_total) {
+                    if (newValue) {
+                        result = price.base_grand_total;
+                    } else {
+                        result = price.base_grand_total;
+                    }
+                    self._changePriceInDom(Math.round(result * 100));
+                }
+            },
+
+            _changePriceInDom: function(price) {
+                const elements = document.querySelectorAll('.nelo-as-low-as');
+                for (let i = 0; i < elements.length; i++) {
+                    elements[i].setAttribute('data-amount', price);
+                }
+            },
+
+            _loadScripts: function() {
+                const head = document.getElementsByTagName('head')[0];
+
+                const configScript = document.createElement('script');
+                const publishableApiKey = window.checkoutConfig.payment.bnpl.publishableApiKey;
+                const isSandboxMode = window.checkoutConfig.payment.bnpl.isSandboxMode;
+                configScript.type = 'text/javascript';
+                configScript.text = '_neloConfig={' +
+                'publishableKey: \' ' + publishableApiKey + '\', ' +
+                'environment: \'' + isSandboxMode ? 'sandbox' : 'production' + '\'' +
+                    '};';
+                head.appendChild(configScript);
+
+                const neloJsScript = document.createElement('script');
+                neloJsScript.type = 'text/javascript';
+                neloJsScript.src = 'https://js.nelo.co/v1/nelo.js';
+                head.appendChild(neloJsScript);
+            },
 
             /**
              * @param {Function} handler
